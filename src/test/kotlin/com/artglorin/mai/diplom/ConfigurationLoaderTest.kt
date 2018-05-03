ea2139ab6654e866545f1cda2adfbe80afc6633c
vverminskiy <develop@artglorin.com>
2018-05-03 11:02:29 +0300

    ConfigurationLoaderTest was fixed

---
+++ src/test/kotlin/com/artglorin/mai/diplom/ConfigurationLoaderTest.kt
@@ -0,0 +1,55 @@
+package com.artglorin.mai.diplom
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * @author V.Verminskiy (vverminskiy@alfabank.ru)
+ * @since 01/05/2018
+ */
+@Tag(Category.UNIT)
+internal class ConfigurationLoaderTest {
+
+    @Test
+    fun `test fail when source does not exist`() {
+        assertThrows(ConfigurationNotLoaded::class.java, {
+            val path = TempFile(file = Paths.get("test.json"))
+            load(path.file)
+        })
+    }
+
+    @Test
+    fun `test fail when source is exist but is not a json structure`() {
+        assertThrows(ConfigurationNotLoaded::class.java, {
+            val path = TempFile()
+            load(path.file)
+        })
+    }
+
+    @Test fun `test that APP_CONFIG file is loaded without issue when configuration file is not specified`() {
+        assertNotNull(ConfigurationLoader.APP_CONFIG.loadProperties())
+    }
+
+    private fun load(path: Path) = ConfigurationLoader({ path.toUri() }).loadProperties()
+
+    @Test
+    fun `test correct read value`() {
+        val configuration = Configuration(Modules("/test/modules"))
+        val tempFile = TempFile()
+        tempFile.save(configuration)
+        val loadedConfiguration = load(tempFile.file)
+        assertEquals(configuration, loadedConfiguration)
+    }
+
+    private data class TempFile(val file: Path = Files.createTempFile(null, ".json")) {
+
+        fun save(it: Any) {
+            ObjectMapper().writeValue(file.toFile(), it)
+        }
+    }
+}
