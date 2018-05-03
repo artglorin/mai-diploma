69af9435b545b6e6c44826a8b8c1926e04b4dc64
vverminskiy <develop@artglorin.com>
2018-05-03 10:48:45 +0300

    ConfigurationLoader was moved to Configuration.kt file

--- src/main/kotlin/com/artglorin/mai/diplom/Configuration.kt
+++ src/main/kotlin/com/artglorin/mai/diplom/Configuration.kt
@@ -1,7 +1,13 @@
 package com.artglorin.mai.diplom
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.core.io.ClassPathResource
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /**
  * @author V.Verminskiy (vverminskiy@alfabank.ru)
@@ -25,3 +31,44 @@ data class ModuleConfig(var jarName: String = "",
                         var settings: JsonNode = ObjectMapper().createObjectNode().nullNode(),
                         var id: String = ""
 )
+
+class ConfigurationLoader(private val source: () -> URI) {
+    companion object {
+        val LOG = LoggerFactory.getLogger(Application::class.java.name)!!
+        val APP_CONFIG = ConfigurationLoader({
+            val configFile = Paths.get(System.getProperty("user.dir"), FilesAndFolders.CONFIG_FILE)
+            if (Files.exists(configFile)) {
+                return@ConfigurationLoader configFile.toUri()
+            } else {
+                val resource = ClassPathResource(FilesAndFolders.CONFIG_FILE)
+                if (resource.exists()) {
+                    return@ConfigurationLoader resource.uri
+                } else {
+                    return@ConfigurationLoader Files.createTempFile(null, null).toUri()
+                }
+            }
+        })
+    }
+
+    private var loaded = false
+    private var configuration: Configuration? = null
+    @Synchronized
+    fun loadProperties(): Configuration {
+        if (!loaded) {
+            try {
+                val mapper = ObjectMapper()
+                mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                configuration = mapper.readValue(source().toURL(), Configuration::class.java)
+                if (configuration == null) {
+                    LOG.error(::ConfigurationNotLoaded, "Configuration was not loaded. May be ${FilesAndFolders.CONFIG_FILE} is have wrong structure")
+                }
+            } catch (e: Exception) {
+                LOG.error(::ConfigurationNotLoaded, "Configuration was not loaded. May be ${FilesAndFolders.CONFIG_FILE} is have wrong structure. Error: $e")
+            }
+            loaded = true
+        }
+
+        return configuration!!
+
+    }
+}
