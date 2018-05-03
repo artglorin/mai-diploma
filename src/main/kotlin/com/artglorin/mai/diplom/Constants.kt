2931c1190277c5278df47751665b3b38e760f6cf
vverminskiy <develop@artglorin.com>
2018-05-03 10:20:32 +0300

    Constant files was changed

--- src/main/kotlin/com/artglorin/mai/diplom/Constants.kt
+++ src/main/kotlin/com/artglorin/mai/diplom/Constants.kt
@@ -7,10 +7,10 @@ package com.artglorin.mai.diplom
 
 object FilesAndFolders {
     const val MODULES_DIR = "modules"
-    const val DATA_SOURCES_MODULE_DIR = "data_sources"
+    const val DATA_SOURCES_MODULE_DIR = "dataSources"
+    const val DATA_HANDLERS_DIR = "dataHandlers"
+    const val SOLUTION_MODULE_DIR = "solution"
+    const val TASK_MANAGER_MODULE_DIR = "taskManager"
+    const val DATA_OBSERVERS_DIR = "dataObservers"
     const val CONFIG_FILE = "app_config.json"
-}
-
-object ConfigKeys {
-    const val MODULES_DIR = "modules_dir"
 }
