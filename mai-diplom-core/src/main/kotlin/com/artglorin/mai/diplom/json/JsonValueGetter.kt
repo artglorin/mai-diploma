7cc050e57090112c32fe779699810d7f263a5628
vverminskiy <develop@artglorin.com>
2018-05-14 10:36:50 +0300

    Extract algorithm was improved

--- mai-diplom-core/src/main/kotlin/com/artglorin/mai/diplom/json/JsonValueGetter.kt
+++ mai-diplom-core/src/main/kotlin/com/artglorin/mai/diplom/json/JsonValueGetter.kt
@@ -2,7 +2,7 @@ package com.artglorin.mai.diplom.json
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
-import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.fasterxml.jackson.databind.node.MissingNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 
 object JsonValueGetter {
@@ -16,27 +16,32 @@ class JsonFieldGetterFactory {
         private val indexMatcher = Regex(".*?\\[(\\d+?)]")
 
         fun create(name: String): JsonFieldGetter {
-            return CompositeGetter(name.split(".").map(::simpleCreate))
+            return split(name)
         }
 
-        private fun simpleCreate(name: String): JsonFieldGetter {
-            val fieldName: String
-            val index: Int?
-            val matchResult = indexMatcher.matchEntire(name)
-            return if (matchResult != null) {
-                index = matchResult.groups[1]?.value?.toInt()
-                fieldName = name.substringBeforeLast("[")
-                return if (fieldName.isNotBlank()) {
-                    if (index != null) {
-                        CompositeGetter(listOf(ObjectGetter(fieldName), ArrayGetter(index)))
-                    } else {
-                        ObjectGetter(fieldName)
-                    }
-                } else if (index != null) {
-                    ArrayGetter(index)
-                } else MissGetter()
-            } else {
-                ObjectGetter(name)
+        private data class Description (val value: String, val type: ContainerType)
+
+        private fun split(income: String) : JsonFieldGetter {
+            val descriptions = ArrayList<Description>()
+            for (it in income.split(".").filter { it.isNotBlank() }) {
+                val index: String? =  indexMatcher.matchEntire(it)?.groups?.get(1)?.value
+                val fieldName: String = if (index != null) it.substringBeforeLast("[") else it
+                if (fieldName.isNotBlank()) {
+                    descriptions.add(Description(fieldName, ObjectType))
+                }
+                if (index != null) {
+                    descriptions.add(Description(index, ArrayType))
+                }
+            }
+            val result = descriptions.map (::toGetter)
+            return if (result.size == 1) result.first() else CompositeGetter(result)
+        }
+
+        private fun toGetter(description: Description) : JsonFieldGetter {
+            return when (description.type) {
+                ArrayType -> ArrayGetter(description.value.toInt())
+                ObjectType -> ObjectGetter(description.value)
+                else -> { throw IllegalStateException("Unsupported containerType : ${description.type}") }
             }
         }
     }
@@ -49,19 +54,19 @@ sealed class JsonFieldGetter {
 
 private class ArrayGetter(private val index: Int): JsonFieldGetter() {
     override fun extract(from: JsonNode): JsonNode {
-        if(from.isArray.not()) return MissingNode.missingNode
+        if(from.isArray.not()) return MissingNode.getInstance()
         from as ArrayNode
         return if (from.size() < index -1){
             from.path(index)
         } else {
-            from.get(index)?: MissingNode.missingNode
+            from.get(index)?: MissingNode.getInstance()
         }
     }
 }
 
 private class ObjectGetter(private val name: String): JsonFieldGetter() {
     override fun extract(from: JsonNode): JsonNode {
-        if(from.isObject.not()) return MissingNode.missingNode
+        if(from.isObject.not()) return MissingNode.getInstance()
         from as ObjectNode
         return from.path(name)
     }
@@ -70,19 +75,11 @@ private class ObjectGetter(private val name: String): JsonFieldGetter() {
 private class CompositeGetter(var getters: List<JsonFieldGetter>) :JsonFieldGetter(){
 
     override fun extract(from: JsonNode): JsonNode {
-        var result: JsonNode = getters.first().extract(from)
-        for (getter in getters.drop(1)) {
+        var result: JsonNode = from
+        for (getter in getters) {
             result = getter.extract(result)
         }
         return result
     }
 
-}
-
-private class MissGetter: JsonFieldGetter() {
-    override fun extract(from: JsonNode) = MissingNode.missingNode
-}
-
-private object MissingNode {
-    val missingNode = JsonNodeFactory.instance.objectNode().path("1")!!
 }
