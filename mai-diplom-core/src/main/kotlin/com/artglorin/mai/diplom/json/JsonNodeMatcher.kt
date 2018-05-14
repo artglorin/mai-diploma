6a28477e63dc629febe1a9f358bf95619d8f6232
vverminskiy <develop@artglorin.com>
2018-05-14 21:52:01 +0300

    DoubleGreatThanMatcher wa added

--- mai-diplom-core/src/main/kotlin/com/artglorin/mai/diplom/json/JsonNodeMatcher.kt
+++ mai-diplom-core/src/main/kotlin/com/artglorin/mai/diplom/json/JsonNodeMatcher.kt
@@ -6,60 +6,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 import java.math.BigDecimal
 import java.math.BigInteger
 
-sealed class JsonNodeMatcher {
-    abstract fun match(node: JsonNode) : Boolean
-}
-
-class StringEqualsMatcher(private val string: String): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node.textValue()
-    }
-
-    override fun toString() = "StringEqualsMatcher math by $string"
-}
-
-class IntegerEqualsMatcher(private val string: Int): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node.intValue()
-    }
-}
-
-class BigIntegerEqualsMatcher(private val template: BigInteger): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return template == node.bigIntegerValue()
-    }
-}
-
-class DoubleEqualsMatcher(private val string: Double): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node.doubleValue()
-    }
-}
-
-class BigDecimalEqualsMatcher(private val string: BigDecimal): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node.decimalValue()
-    }
-}
-
-class LongEqualsMatcher(private val string: Long): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node.longValue()
-    }
-}
-
-class ArrayEqualsMatcher(private val string: ArrayNode): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node
-    }
-}
-
-class ObjectEqualsMatcher(private val string: ObjectNode): JsonNodeMatcher() {
-    override fun match(node: JsonNode): Boolean {
-        return string == node
-    }
-}
-
 object JsonNodeMatcherFactory {
     private val types: Collection<String> = JsonType.values().map { it.name }
     /**
@@ -109,22 +55,89 @@ object JsonNodeMatcherFactory {
                 }
             }
 //            "lessThen" -> LessThenTransformerMatcher(settings.path("fromValue"))
-//            "greatThen" -> {
-//                val template = settings.get("template")
-//                when {
+            "greatThen" -> {
+                val template = settings.get("template")
+                when {
 //                    template.isTextual -> StringEqualsMatcher(template.asText())
 //                    template.isInt -> IntegerEqualsMatcher(template.asInt())
 //                    template.isBigInteger -> BigIntegerEqualsMatcher(template.bigIntegerValue())
 //                    template.isLong -> LongEqualsMatcher(template.asLong())
-//                    template.isDouble -> DoubleEqualsMatcher(template.asDouble())
+                    template.isDouble -> DoubleGreatThanMatcher(template.asDouble())
 //                    template.isBigDecimal -> BigDecimalEqualsMatcher(template.decimalValue())
 //                    template.isArray -> ArrayEqualsMatcher(template as ArrayNode)
 //                    template.isObject -> ObjectEqualsMatcher(template as ObjectNode)
-//                    else -> throw IllegalArgumentException("Unsupported json type: ${template.nodeType}. Supported values: $types")
-//                }
-//            }
+                    else -> throw IllegalArgumentException("Unsupported json type: ${template.nodeType}. Supported values: $types")
+                }
+            }
 //            "between" -> BetweenTransformerMatcher(settings.path("from"), settings.path("to"))
             else -> throw IllegalArgumentException("Unsupported TransformerMatcher type: '$name'")
         }
+    }
+}
+
+
+sealed class JsonNodeMatcher {
+    abstract fun match(node: JsonNode): Boolean
+}
+
+//<editor-fold desc="EQUALS">
+class StringEqualsMatcher(private val string: String) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node.textValue()
+    }
+
+    override fun toString() = "StringEqualsMatcher math by $string"
+}
+
+class IntegerEqualsMatcher(private val string: Int) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node.intValue()
+    }
+}
+
+class BigIntegerEqualsMatcher(private val template: BigInteger) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return template == node.bigIntegerValue()
+    }
+}
+
+class DoubleEqualsMatcher(private val string: Double) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node.doubleValue()
+    }
+}
+
+class BigDecimalEqualsMatcher(private val string: BigDecimal) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node.decimalValue()
+    }
+}
+
+class LongEqualsMatcher(private val string: Long) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node.longValue()
+    }
+}
+
+class ArrayEqualsMatcher(private val string: ArrayNode) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node
+    }
+}
+
+class ObjectEqualsMatcher(private val string: ObjectNode) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return string == node
+    }
+}
+//</editor-fold>
+
+class DoubleGreatThanMatcher(private val template: Double) : JsonNodeMatcher() {
+    override fun match(node: JsonNode): Boolean {
+        return template < node.doubleValue()
+    }
+
+    override fun toString(): String {
+        return "DoubleGreatThanMatcher($template)"
     }
 }
