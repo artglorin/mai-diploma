41650e43e0d10ac0bc10716a72e54a5ba82280d6
vverminskiy <develop@artglorin.com>
2018-05-19 12:06:03 +0300

    DataListenersModules.kt was created

---
+++ mai-diplom-synoptic-modules/src/main/kotlin/com/artglorin/mai/diplom/synoptic/modules/datalisteners/DataListenersModules.kt
@@ -0,0 +1,43 @@
+package com.artglorin.mai.diplom.synoptic.modules.datalisteners
+
+import com.artglorin.mai.diplom.core.api.Customizable
+import com.artglorin.mai.diplom.core.api.DataObserver
+import com.artglorin.mai.diplom.json.JsonSchemaBuilder
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.springframework.core.io.ClassPathResource
+import java.io.File
+
+/**
+ * @author V.Verminskiy (vverminskiy@alfabank.ru)
+ * @since 18/05/2018
+ */
+
+class FileOutputModule: Customizable, DataObserver {
+    private var file: File? = null
+    override fun getInputSchema(): ObjectNode {
+        return JsonSchemaBuilder().build()
+    }
+
+    override fun push(node: ObjectNode) {
+        file?.appendText(node.asText())
+    }
+
+    override fun applySettings(settings: JsonNode) {
+        file = settings.get("file").textValue()?.let {
+            ClassPathResource(it).file
+        }?.apply {
+            createNewFile()
+        }
+    }
+}
+
+class ConsoleOutputModule: DataObserver {
+    override fun getInputSchema(): ObjectNode {
+        return JsonSchemaBuilder().build()
+    }
+
+    override fun push(node: ObjectNode) {
+        println(node)
+    }
+}
